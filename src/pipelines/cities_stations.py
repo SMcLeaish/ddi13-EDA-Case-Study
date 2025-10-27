5cdb166fb922c6988c276fc1b8e6c43d97efc68a
Sean McLeaish <smcleaish@gmail.com>
2025-10-27 10:41:21 -0400

    static methods

--- src/pipelines/cities_stations.py
+++ src/pipelines/cities_stations.py
@@ -6,7 +6,10 @@ import polars as pl
 
 class CitiesStations:
     def __init__(
-        self, country: str, cities_csv: Path | str, stations_csv: Path | str
+        self,
+        cities_csv: Path | str = '../data/cities.csv',
+        stations_csv: Path | str = '../data/stations.csv',
+        country: str = 'United States',
     ):
         self.cities: pl.LazyFrame = pl.scan_csv(cities_csv)
         self.stations: pl.LazyFrame = pl.scan_csv(stations_csv)
@@ -16,63 +19,17 @@ class CitiesStations:
 
     def _build_ldf(self) -> pl.LazyFrame:
         return (
-            self.filter_cities_by_country(self.cities, self.country)
-            .pipe(lambda ldf: self.join_on_city_id(self.stations, ldf))
-            .pipe(self.rename_name_right_to_city)
-            .pipe(self.rename_buildstart_to_start)
-            .pipe(self.drop_empty_country_rows)
-            .pipe(self.drop_empty_start)
-            .pipe(self.change_start_to_datetime)
+            self._filter_cities_by_country(self.cities, self.country)
+            .pipe(lambda ldf: self._join_on_city_id(self.stations, ldf))
+            .pipe(self._rename_name_right_to_city)
+            .pipe(self._rename_buildstart_to_start)
+            .pipe(self._drop_empty_country_rows)
+            .pipe(self._drop_empty_start)
+            .pipe(self._change_start_to_datetime)
         )
-
-    def _collect_df(
-        self, ldf: pl.LazyFrame, reset: bool = True
-    ) -> pl.DataFrame:
-        if reset or self.df is None:
-            self.df = ldf.collect()
-        return self.df
 
     def show_graph(self) -> None:
         self.ldf.show_graph()
-
-    def _start_count(self) -> pl.LazyFrame:
-        return (
-            self.ldf.group_by(['city', 'start'])
-            .agg(pl.len())
-            .sort(['city', 'start'])
-            .with_columns(
-                pl.col('len').cum_sum().over('city').alias('cumulative')
-            )
-        )
-
-    def filter_cities_by_country(
-        self, ldf: pl.LazyFrame, country: str
-    ) -> pl.LazyFrame:
-        return ldf.filter(pl.col('country') == country)
-
-    def join_on_city_id(
-        self, left_ldf: pl.LazyFrame, right_ldf: pl.LazyFrame
-    ) -> pl.LazyFrame:
-        return left_ldf.join(
-            right_ldf, left_on='city_id', right_on='id', how='left'
-        )
-
-    def drop_empty_country_rows(self, ldf: pl.LazyFrame) -> pl.LazyFrame:
-        return ldf.drop_nulls(subset='country')
-
-    def rename_buildstart_to_start(self, ldf: pl.LazyFrame) -> pl.LazyFrame:
-        return ldf.rename({'buildstart': 'start'})
-
-    def rename_name_right_to_city(self, ldf: pl.LazyFrame) -> pl.LazyFrame:
-        return ldf.rename({'name_right': 'city'})
-
-    def drop_empty_start(self, ldf: pl.LazyFrame) -> pl.LazyFrame:
-        return ldf.filter(pl.col('start') > 0)
-
-    def change_start_to_datetime(self, ldf: pl.LazyFrame) -> pl.LazyFrame:
-        return ldf.with_columns(
-            pl.datetime(pl.col('start'), 1, 1).alias('date_start')
-        )
 
     def line_chart(self, file: str | None = None) -> alt.Chart:
         ldf = self._start_count()
@@ -80,7 +37,7 @@ class CitiesStations:
         chart: alt.Chart = df.plot.line(
             x='start', y='cumulative', color='city'
         )
-        if file:
+        if isinstance(file, str):
             chart.save(file)  # type: ignore
         return chart
 
@@ -113,3 +70,56 @@ class CitiesStations:
         if file:
             chart.save(file)  # type: ignore
         return chart
+
+    def _collect_df(
+        self, ldf: pl.LazyFrame, reset: bool = True
+    ) -> pl.DataFrame:
+        if reset or self.df is None:
+            self.df = ldf.collect()
+        return self.df
+
+    def _start_count(self) -> pl.LazyFrame:
+        return (
+            self.ldf.group_by(['city', 'start'])
+            .agg(pl.len())
+            .sort(['city', 'start'])
+            .with_columns(
+                pl.col('len').cum_sum().over('city').alias('cumulative')
+            )
+        )
+
+    @staticmethod
+    def _filter_cities_by_country(
+        ldf: pl.LazyFrame, country: str
+    ) -> pl.LazyFrame:
+        return ldf.filter(pl.col('country') == country)
+
+    @staticmethod
+    def _join_on_city_id(
+        left_ldf: pl.LazyFrame, right_ldf: pl.LazyFrame
+    ) -> pl.LazyFrame:
+        return left_ldf.join(
+            right_ldf, left_on='city_id', right_on='id', how='left'
+        )
+
+    @staticmethod
+    def _drop_empty_country_rows(ldf: pl.LazyFrame) -> pl.LazyFrame:
+        return ldf.drop_nulls(subset='country')
+
+    @staticmethod
+    def _rename_buildstart_to_start(ldf: pl.LazyFrame) -> pl.LazyFrame:
+        return ldf.rename({'buildstart': 'start'})
+
+    @staticmethod
+    def _rename_name_right_to_city(ldf: pl.LazyFrame) -> pl.LazyFrame:
+        return ldf.rename({'name_right': 'city'})
+
+    @staticmethod
+    def _drop_empty_start(ldf: pl.LazyFrame) -> pl.LazyFrame:
+        return ldf.filter(pl.col('start') > 0)
+
+    @staticmethod
+    def _change_start_to_datetime(ldf: pl.LazyFrame) -> pl.LazyFrame:
+        return ldf.with_columns(
+            pl.datetime(pl.col('start'), 1, 1).alias('date_start')
+        )
